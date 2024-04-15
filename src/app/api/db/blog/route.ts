4cfd8958f94ce75933bb6302c388a61ecc77e31a
gonwms <gon.williams@gmail.com>
2024-04-15 19:01:32 -0300

    .

--- src/app/api/db/blog/route.ts
+++ src/app/api/db/blog/route.ts
@@ -1,30 +1,30 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
+// import { sql } from '@vercel/postgres'
+// import { NextResponse } from 'next/server'
 
-// GET POST ----------------------------------------------------------------
-export async function GET(): Promise<void | Response> {
-  try {
-    const result = await sql`SELECT * FROM blog;`
-    return NextResponse.json({ result }, { status: 200 })
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
-  }
-}
+// // GET POST ----------------------------------------------------------------
+// export async function GET(): Promise<void | Response> {
+//   try {
+//     const result = await sql`SELECT * FROM blog;`
+//     return NextResponse.json({ result }, { status: 200 })
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 })
+//   }
+// }
 
-// POST  ----------------------------------------------------------------
-interface PostRequest {
-  title: string
-  slug: string
-}
-export async function POST(request: any): Promise<void | Response> {
-  const { title, slug }: PostRequest = await request.json()
-  try {
-    if (!title || !slug) return NextResponse.json('title and slug required', { status: 5500 })
-    await sql`INSERT INTO blog (title, slug) VALUES (${title}, ${slug});`
+// // POST  ----------------------------------------------------------------
+// interface PostRequest {
+//   title: string
+//   slug: string
+// }
+// export async function POST(request: any): Promise<void | Response> {
+//   const { title, slug }: PostRequest = await request.json()
+//   try {
+//     if (!title || !slug) return NextResponse.json('title and slug required', { status: 5500 })
+//     await sql`INSERT INTO blog (title, slug) VALUES (${title}, ${slug});`
 
-    const result = await sql`SELECT * FROM blog;`
-    return NextResponse.json({ result }, { status: 200 })
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
-  }
-}
+//     const result = await sql`SELECT * FROM blog;`
+//     return NextResponse.json({ result }, { status: 200 })
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 })
+//   }
+// }
--- src/app/api/db/consentproof/route.ts
+++ src/app/api/db/consentproof/route.ts
@@ -1,19 +1,19 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
+// import { sql } from '@vercel/postgres'
+// import { NextResponse } from 'next/server'
 
-interface PostRequest {
-  proof: string
-}
-export async function POST(request: any): Promise<void | Response> {
-  const { proof }: PostRequest = await request.json()
-  try {
-    if (!proof) return NextResponse.json('data required', { status: 500 })
-    //
-    await sql`INSERT INTO consentproof (Data) VALUES (${proof});`
+// interface PostRequest {
+//   proof: string
+// }
+// export async function POST(request: any): Promise<void | Response> {
+//   const { proof }: PostRequest = await request.json()
+//   try {
+//     if (!proof) return NextResponse.json('data required', { status: 500 })
+//     //
+//     await sql`INSERT INTO consentproof (Data) VALUES (${proof});`
 
-    const result = await sql`SELECT * FROM consentproof;`
-    return NextResponse.json({ result }, { status: 200 })
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
-  }
-}
+//     const result = await sql`SELECT * FROM consentproof;`
+//     return NextResponse.json({ result }, { status: 200 })
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 })
+//   }
+// }
