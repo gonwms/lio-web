96f778415abc38762842d105c36b9df48887834d
gonwms <gon.williams@gmail.com>
2024-06-21 23:14:17 -0300

    title description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,16 @@ export async function metadata() {
     googlebot: "index, follow",
     author: "LIO",
     title: `LIO ~ Los Inorgánicos Organizados`,
-    description: "Movimiento peronistaA",
+    description:
+      "Somos LIO, una organización comprometida con los ideales del Movimiento Peronista, enfocada en la justicia social, la igualdad y el bienestar del pueblo argentino. Nos alineamos con el partido político Principios y Valores, bajo la conducción de Guillermo Moreno.",
     icons: { icon: "/favicon.png" },
     keywords:
       "LIO, Los Inorganicos Organizados, Peronismo, Guillermo Moreno, Nydia Lirola, ",
 
     openGraph: {
-      title: `LIO | Los inorgánicos organizados`,
-      description: "Movimiento peronista",
+      title: `LIO ~ Los Inorgánicos Organizados`,
+      description:
+        "Somos LIO, una organización comprometida con los ideales del Movimiento Peronista, enfocada en la justicia social, la igualdad y el bienestar del pueblo argentino. Nos alineamos con el partido político Principios y Valores, bajo la conducción de Guillermo Moreno.",
       type: "website",
       url: "https://www.lio.com.ar",
       images: ["https://www.lio.com.ar/cover.webp"],
@@ -33,7 +35,8 @@ export async function metadata() {
     twitter: {
       card: "summary_large_image",
       title: `LIO ~ Los Inorgánicos Organizados`,
-      description: "Movimiento peronista",
+      description:
+        "Somos LIO, una organización comprometida con los ideales del Movimiento Peronista, enfocada en la justicia social, la igualdad y el bienestar del pueblo argentino. Nos alineamos con el partido político Principios y Valores, bajo la conducción de Guillermo Moreno.",
       site: "@Hacemos_Lio",
       url: "https://www.lio.com.ar",
       images: "https://www.lio.com.ar/cover.webp",
