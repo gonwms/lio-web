2c7ea0d2f3adfa301f7fd1599a124b343199ee55
gonwms <gon.williams@gmail.com>
2024-07-13 20:58:57 -0300

    feat{scope}: api sitemap xml :
    {breaking_change}{footer}

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -1,19 +1,27 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  // return NextResponse.json("data")
+export async function GET() {
   try {
+    // Fetch the XML sitemap from Strapi
     const response = await fetch("http://127.0.0.1:1337/api/sitemap/index.xml")
-    console.log(response)
-    const sitemapXml = await response.text()
-    // return NextResponse.json(sitemapXml)
+    if (!response.ok) {
+      throw new Error("Failed to fetch the sitemap from Strapi")
+    }
+
+    // Get the XML text from the response
+    let sitemapXml = await response.text()
+
+    // Remove any XSL references if present
+    sitemapXml = sitemapXml.replace(/<\?xml-stylesheet [^>]*\?>/g, "")
+
+    // Return the XML response
     return new NextResponse(sitemapXml, {
       headers: {
         "Content-Type": "application/xml",
       },
     })
   } catch (error) {
-    console.error(error)
-    res
+    console.error("Failed to fetch the sitemap:", error)
+    return new NextResponse("Failed to fetch the sitemap", { status: 500 })
   }
 }
