a6526e331a886dcc63d359411b92b9a7b139591b
gonwms <gon.williams@gmail.com>
2024-11-01 14:29:49 -0300

    remove radix theme

--- src/app/(pages)/quienes-somos/page.tsx
+++ src/app/(pages)/quienes-somos/page.tsx
@@ -2,7 +2,7 @@
 import React, { useRef } from "react"
 import classNames from "classnames"
 import AdmisionForm from "@/components/forms/AdminisionForm"
-import { Section } from "@radix-ui/themes"
+
 import { Row } from "@/components/CustomTags"
 
 // import styles from './contact.module.scss'
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-// import { Flex } from "@radix-ui/themes"
+
 import React from "react"
 import styles from "./sidebar.module.scss"
 import Link from "next/link"
