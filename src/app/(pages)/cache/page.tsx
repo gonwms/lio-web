f98fc49b01d4ddb76ddc0004bddc906a19b4b7c6
gonwms <gon.williams@gmail.com>
2024-12-01 04:00:36 -0300

    share btn

--- src/app/(pages)/cache/page.tsx
+++ src/app/(pages)/cache/page.tsx
@@ -1,12 +1,19 @@
-import React from "react"
-import { manualClearCache } from "@/actions"
-import styles from "./cache.module.scss"
-import Link from "next/link"
+'use client'
+import React, { useState } from 'react'
+import { manualClearCache } from '@/actions'
+import styles from './cache.module.scss'
+import Link from 'next/link'
 
 export default function Clear({ searchParams }: any) {
   // const clearedPathName = searchParams.get("clearedPathName")
   // console.log("searchParams:", searchParams)
+  const baseURL = 'https://www.lio.com/'
+  const [valor, setValor] = useState(baseURL)
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setValor(value)
+  }
   return (
     <div>
       <h2>Actualizar cache</h2>
@@ -19,12 +26,14 @@ export default function Clear({ searchParams }: any) {
           name="name"
           type="url"
           placeholder="https://www.lio.com/recursos/la-marcha-peronista"
+          value={valor}
+          onChange={(e) => handleChange(e)}
         />
         <button type="submit">actualizar</button>
       </form>
       {searchParams?.clearedPathName && (
         <p>
-          se actualizó:{" "}
+          se actualizó:{' '}
           <Link
             rel="stylesheet"
             href={searchParams?.clearedPathName}
